ce5b7eb0ce719fbf0b5ff732549f542cde272b7d
Devyn Datson <ddatson1@mail.valenciacollege.edu>
2016-01-19 14:36:25 -0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,22 @@
 # 2. asks the user to choose from 1 of 3 spoken languages (pick your favorite languages!) 
 # 3. displays the greeting in the chosen language
 # 4. exits
+print('Hello User!')
+print('Please choose a preffered language') #Prompting user and giving them a list to choose from.
+print('1.Icelandic')
+print('2.Polish')
+print('3.Swedish')
+lang = input()
+lang1 = print('kveðjur')
+lang2 = print('Halo')
+lang3 = print('Hallå')
+if lang = 1 print lang1
+if lang = 2 print lang2
+if lang = 3 print lang3
+
+
+
+
+exit()
 
 # make sure that your code contains comments explaining your logic!
-
